d25071a2dd538be5124b5a506947cce5c65ca434
cathytanya <cathycolette.tanya@gmail.com>
2021-09-22 02:36:27 -0400

    function will present the last answer

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 // selecting all the elements
 let start = document.getElementById("start");
 let question = document.getElementById("question");
-let questionElement = document.getElementById("questionElement");
-let optionsElement = document.getElementById("options")
-let answerElement = document.getElementById("answerElement");
+let questionEl = document.getElementById("questionElement");
+let optionsEl = document.getElementById("optionsElement")
+let answerEl = document.getElementById("answersElement");
 let timer = document.getElementById("timer");
 let highScore = document.getElementById("highScore");
 
 let questions = [
     {
         // Question 1
-        questionElement: 'Which element is nonsemantic?',
-        optionsElement: ['div','form','header','footer'],
-        answerElement: 'div'
+        statement: 'Which element is nonsemantic?',
+        choice: ['div','form','header','footer'],
+        right: 'div'
     },
     {
         // question 2
-        questionElement: 'What is the purpose of  HTML?',
-        optionsElement: ['Basic structure of website','Used to control presentation','Used to control the behaviour of different elements','Task automation'],
-        answerElement: 'Basic structure of website'
+        statement: 'What is the purpose of  HTML?',
+        choice: ['Basic structure of website','Used to control presentation','Used to control the behaviour of different elements','Task automation'],
+        right: 'Basic structure of website'
     },
     {
         // question 3
         questionElement: 'What is the purpose of  CSS?',
         optionsElement: ['Basic structure of website','Used to control presentation','Used to control the behaviour of different elements','Task automation'],
-        answerElement: 'Used to control presentation'
+        right: 'Used to control presentation'
     },
     {
         // question 4
         questionElement: 'What is the purpose of  JavaScript?',
         optionsElement: ['Basic structure of website','Used to control presentation','Used to control the behaviour of different elements','Task automation'],
-         answer: 'Used to control the behaviour of different elements'
+        right: 'Used to control the behaviour of different elements'
     },
     {
         // question 5
         questionElement: 'What does "ppl" represent in the following? ppl = ["Bob","Sam","Jill","Jo","Shawn"]',
         optionsElement: ['boolean','array','string','variable'],
-        answerElement: 'array'
+        right: 'array'
     },
     {
         // question 6
         questionElement: 'What is a "true" element?',
         optionsElement: ['boolean','array','string','variable'],
-        answerElement: 'boolean'
+        right: 'boolean'
     },
     {
         // quesiton 7
         questionElement: 'What are: ".matches",".spice",".push",".getElementById"?',
         optionsElement: ['Object','Method','Element','Function'],
-        answerElement: 'Method'
+        right: 'Method'
     },
 ]
 
@@ -56,31 +56,39 @@ let questions = [
 function startBtn (){
     let quizRulesBox = document.getElementById("quizRulesBox");
     quizRulesBox.setAttribute("style", "display: none");
-    setTime();
+    // setTime();
 }
+// function presentQuestion(){
+// questionEl.textContent = questions[0].statement;
+// console.log(question)
+// }
 
-function startQuiz(questionElement){
-    for (let i=0; i<questions.length; i++){
-        
-
-
+// function presentOptions(){
+// optionsEl.textContent = questions[0].choice
+// }
+function present(){
+    for(let i=0;i < questions.length;i++) {
+    questionEl.textContent = questions[i].statement;
+    optionsEl.textContent = questions[i].choice;
+    answerEl.textContent = questions[i].right
     }
 }
 
-let timerLeft = 60;
+present();
+// let timerLeft = 60;
 
-function setTime() {
-  let timerInterval = setInterval(function() {
-    timerLeft--;
-    timer.textContent = "Time: " + timerLeft + " sec left";
-    if(timerLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
-}
-function sendMessage() {
-  timer.textContent = " ";
-}
+// function setTime() {
+//   let timerInterval = setInterval(function() {
+//     timerLeft--;
+//     timer.textContent = "Time: " + timerLeft + " sec left";
+//     if(timerLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+//   }, 1000);
+// }
+// function sendMessage() {
+//   timer.textContent = " ";
+// }
 
 start.onclick = startBtn;
